c9c22fda8100bfbae8361320c65577ddb56f6589
Can Duc Khoi <133144430+theflyingkhui04@users.noreply.github.com>
2026-05-06 16:04:09 +0700

    fix name

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routers import ingestion_api, search_api, smart_api
+from core.ingestion.indexer import global_index
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Đang khởi động TNKV DB...")
+    print("Đang nạp dữ liệu Inverted Index từ ổ cứng lên RAM...")
+    global_index.load_from_disk("storage")
+    print(f"Đã tải thành công {global_index.total_documents} tài liệu vào bộ nhớ.")
+    
+    yield
+    
+    # --- Sự kiện Shutdown ---
+    print("Đang tắt TNKV DB. Đang lưu trạng thái cuối cùng xuống đĩa...")
+    global_index.save_to_disk("storage")
+    print("Đã lưu thành công.")
 
 app = FastAPI(
     title="TNKV DB", 
-    description="The Lightweight Sparse Vector Database for IR Purists",
-    version="1.0.0"
+    description="Vector Database thưa do nhóm của Khôi, Nhật, Tùng làm trong môn IR",
+    version="1.0.0",
+    lifespan=lifespan
 )
 
-# Đăng ký các module routers
 app.include_router(ingestion_api.router)
 app.include_router(search_api.router)
 app.include_router(smart_api.router)
 
+
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to TNKV DB API! Access /docs for Swagger UI."}
+    return {"message": "Xin chào!"}
